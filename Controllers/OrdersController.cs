8764c5e80f66c76d7551414b68144860b43f7915
katherinevfry <katyvfry@gmail.com>
2021-10-16 13:59:13 -0500

    add get requests

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using scapegoat.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scapegoat.Controllers
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        OrdersRepository _repo;
+        public OrdersController(OrdersRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllOrders()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingleOrder(Guid id)
+        {
+            return Ok(_repo.GetById(id));
+        }
+
+        [HttpGet("/userorder/{UserId}")]
+        public IActionResult GetOrderByUserId(Guid UserId)
+        {
+            return Ok(_repo.GetByUserId(UserId));
+        }
+    }
+}
---
+++ DataAccess/OrdersRepository.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using scapegoat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scapegoat.DataAccess
+{
+    public class OrdersRepository
+    {
+        readonly string _connectionString;
+        public OrdersRepository(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("Scapegoat");
+        }
+
+        internal IEnumerable<Order> GetAll()
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var sqlString = @"select * from Orders";
+
+            var orders = db.Query<Order>(sqlString);
+
+            return orders;
+        }
+
+        internal Order GetById(Guid id)
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var sqlString = @"select * from Orders where Id = @id";
+
+            var order = db.QueryFirstOrDefault<Order>(sqlString, new { id = id } );
+
+            if (order == null) return null;
+
+            return order;
+        }
+
+        internal IEnumerable<Order> GetByUserId(Guid userId)
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var sqlString = @"select * from Orders where UserId = @UserId";
+
+            var orders = db.Query<Order>(sqlString, new { UserId = userId });
+
+            return orders;
+        }
+    }
+}
