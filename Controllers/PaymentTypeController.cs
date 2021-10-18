e7ce95489cd9ac6684a1ba5c4e1177569baed3c6
Mitchell-Crumbley <mitchellcrumbley@gmail.com>
2021-10-18 17:41:58 -0500

    Adding controllers models and repo

---
+++ Controllers/PaymentTypeController.cs
@@ -0,0 +1,46 @@
+﻿using scapegoat.DataAccess;
+using scapegoat.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scapegoat.Controllers
+{
+    [Route("api/paymentTypes")]
+    [ApiController]
+    public class PaymentTypeController : ControllerBase
+    {
+        PaymentTypeRepository _repo;
+        public PaymentTypeController(PaymentTypeRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPaymentTypes()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPaymentTypeById(Guid id)
+        {
+            var paymentType = _repo.GetById(id);
+
+            if (paymentType == null)
+            {
+                return NotFound($"No paymentType with the id {id} was found.");
+            }
+
+            return Ok(paymentType);
+        }
+
+        [HttpGet("paymentMethods/{paymentMethods}")]
+        public IEnumerable<PaymentType> GetPaymentByMethod(PaymentMethod paymentMethod)
+        {
+            return _repo.GetByMethod(paymentMethod);
+        }
+    }
+}
---
+++ DataAccess/PaymentTypeRepository.cs
@@ -0,0 +1,41 @@
+﻿using scapegoat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace scapegoat.DataAccess
+{
+    public class PaymentTypeRepository
+    {
+        string _connectionString;
+        public PaymentTypeRepository(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("Scapegoat");
+        }
+        internal IEnumerable<PaymentType> GetAll()
+        {
+            using var db = new SqlConnection(_connectionString);
+            var sqlString = @"select * from PaymentTypes";
+
+            var paymentTypes = db.Query<PaymentType>(sqlString);
+
+            return paymentTypes;
+        }
+
+        public PaymentType GetByMethod(Guid id)
+        {
+            //create connection 
+            using var db = new SqlConnection(_connectionString);
+            var sqlString = @"select * from PaymentTypes where PaymentMethod = @PaymentMethod";
+
+
+            var paymentType = db.Query<PaymentType>(sqlString, new { id = id });
+
+            return (PaymentType)paymentType;
+        }
+    }
+}
