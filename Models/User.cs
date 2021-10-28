fcf582369b8b290026edc4f9c98170bddf13748c
hmparsons-sde <holly-parsons@outlook.com>
2021-10-27 20:43:14 -0500

    enum serializer working

--- Models/User.cs
+++ Models/User.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace scapegoat.Models
 {
@@ -11,23 +13,34 @@ namespace scapegoat.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime CreatedAt { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public UserType UserType { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public CustomerTier CustomerTier { get; set; }
     }
 
     public enum UserType
     {
+        [EnumMember(Value = "Buyer")]
         Buyer,
+        [EnumMember(Value = "Seller")]
         Seller,
+        [EnumMember(Value = "Removed User")]
         DeletedUser
     }
 
     public enum CustomerTier
     {
+        [EnumMember(Value = "Individual")]
         Individual,
+        [EnumMember(Value = "Small Business")]
         SmallBusiness,
+        [EnumMember(Value = "Mid-sized Business")]
         MediumBusiness,
+        [EnumMember(Value = "Enterprise")]
         Enterprise,
+        [EnumMember(Value = "Removed User")]
         DeletedUser
     }
 }
