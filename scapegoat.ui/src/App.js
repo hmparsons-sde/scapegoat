33696debc81b4e9cab8b0995a1dac510e42e8c16
hmparsons-sde <holly-parsons@outlook.com>
2021-11-13 10:19:38 -0600

    import

--- scapegoat.ui/src/App.js
+++ scapegoat.ui/src/App.js
@@ -1,8 +1,5 @@
-
-import './styles/App.css';
 import React, { useState, useEffect } from 'react';
-import '../src/styles/App.css';
-
+import '../src/App.css';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
