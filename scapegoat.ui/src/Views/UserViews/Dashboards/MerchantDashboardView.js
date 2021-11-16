c76034cf5a4733b7ee3c18d8a57a735350039a49
katherinevfry <katyvfry@gmail.com>
2021-11-15 20:46:55 -0600

    merchant dash ui

--- scapegoat.ui/src/Views/UserViews/Dashboards/MerchantDashboardView.js
+++ scapegoat.ui/src/Views/UserViews/Dashboards/MerchantDashboardView.js
@@ -12,7 +12,47 @@ display: flex;
 flex-direction: row;
 flex-wrap: wrap;
 justify-content: center;
+padding: 2rem;
 `;
+
+const FormButton = styled.div`
+  .button_slide {
+    color: black;
+    border: 2px solid #e7e7e7;
+    border-radius: 0px;
+    padding: 18px 36px;
+    display: inline-block;
+    font-size: 14px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    box-shadow: inset 0 0 0 0 #000000;
+    -webkit-transition: ease-out 0.4s;
+    -moz-transition: ease-out 0.4s;
+    transition: ease-out 0.4s;
+  }
+  .slide_down:hover {
+    box-shadow: inset 0 100px 0 0 #000000;
+    color: white;
+  }
+  align-content: center;
+  margin-top: 10px;
+`;
+
+const PageWrapper = styled.div`
+background-size:auto, 100% 100%;
+font-size: 16px;
+line-height: 1.5;
+font-weight: 300;
+min-height: 100%;
+overflow-y: scroll;
+width: 100%;
+margin: 50px;
+
+h1, h3, h4 {
+  font-weight: 300;
+  line-height: 1.2;
+}
+}`;
 
 export default function MerchantDashboardView({user}) {
   const [merchantOrders, setMerchantOrders] = useState([]);
@@ -28,25 +68,36 @@ export default function MerchantDashboardView({user}) {
   }, [updateSwitch, user?.id]);
   
   return (
-    <div>
+    <PageWrapper>
       <h1>merchant dash</h1>
       {
       merchantOrders
-      ? <FlexyDiv>
-        { merchantOrders.map((order) => (
-        <FulfillOrders key={order.id} orders={order}/>
-      ))
-      } 
-      </FlexyDiv>
+      ? <div>
+        <h3>My Orders</h3>
+        <FlexyDiv>
+          { merchantOrders.map((order) => (
+          <FulfillOrders key={order.id} orders={order}/>
+          ))
+          } 
+        </FlexyDiv>
+      </div>
       : null
       }
+      
+      <h3>Metrics</h3>
       <MerchantMetrics merchantOrders={merchantOrders} monthlyOrders={thisMonthOrders} />
-      <h2>My Products</h2>
+
+      <h3>My Products</h3>
       <div>
       {
         addProduct
-        ? <button onClick={() => setAddProduct(!addProduct)}>Cancel</button>
-        : <button outline onClick={() => setAddProduct(!addProduct)}>Add Product</button>
+            ? <FormButton onClick={() => setAddProduct(!addProduct)}><div className="button_slide slide_down">
+            Cancel
+        </div>
+        </FormButton>
+        : <FormButton outline onClick={() => setAddProduct(!addProduct)}><div className="button_slide slide_down">
+        Add Product
+    </div></FormButton>
       }
        {
             addProduct
@@ -77,6 +128,6 @@ export default function MerchantDashboardView({user}) {
       ))} </FlexyDiv>
       : <p>You currently have no products.</p>
       }
-    </div>
+    </PageWrapper>
   )
 }
