cc6601e67577da93830ace305ae4234ca2351e77
hmparsons-sde <holly-parsons@outlook.com>
2021-11-29 17:34:23 -0600

    testy

--- wwwroot/scapegoat.ui/src/Views/UserViews/PleaseLogin.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function PleaseLogin() {
-  return (
-        <>
-          <h1 className="mt-5">Please login to access all of our features.</h1>
-        </>
-  );
-}
